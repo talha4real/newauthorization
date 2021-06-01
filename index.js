6fc30b212a37d96b116a4e413ec055e5f598e95b
Talha <talhabutt993@gmail.com>
2021-06-02 02:41:07 +0500

    finished auth

--- index.js
+++ index.js
@@ -44,13 +44,43 @@ app.post('/login',(req,res)=>{
           res.send('success');
       }).catch(console.error);
 })
-app.get('/profile' ,(req, res) =>{
-    res.render('protectedroute.ejs');
+function checkAuthenticated(req, res, next){
+
+    let token = req.cookies['session-token'];
+
+    let user = {};
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        });
+        const payload = ticket.getPayload();
+        user.name = payload.name;
+        user.email = payload.email;
+        user.picture = payload.picture;
+      }
+      verify()
+      .then(()=>{
+          req.user = user;
+          next();
+      })
+      .catch(err=>{
+          res.redirect('/login')
+      })
+
+}
+app.get('/dashboard',checkAuthenticated,(req, res) =>{
+    let user = req.user;
+    res.render('dashboard',{user});
   
 })
-app.get('/protectedroute' ,(req, res) =>{
-    let user = req.user;
-    res.render('profile',{user});
+app.get('/protectedroute',checkAuthenticated ,(req, res) =>{
+    res.render('protectedroute');
+  
+})
+app.get('/logout' ,(req, res) =>{
+    res.clearCookie('session-token');
+    res.redirect('/login')
   
 })
 
